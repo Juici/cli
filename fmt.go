8af7f38a97120a0bc70ebb7ae8e0fd70f490d994
James Whaley <juicy66173@gmail.com>
2017-11-24 18:33:45 +0000

    rename to help.go -> fmt.go

---
+++ fmt.go
@@ -0,0 +1,18 @@
+package cli
+
+const (
+	// Default number of characters per line.
+	defaultWidth = 74
+
+	// Default padding to the left of flag lines.
+	defaultFlagPad = 2
+
+	// Default padding before flag description.
+	defaultDescPad = 2
+
+	// Usage statement prefix.
+	usagePrefix = "Usage: "
+
+	// Prefix to the flags block.
+	defaultFlagsPrefix = "Flags:"
+)
